f76fdba74e859d8adf9ecc7a614e140c4eeb8a09
Ubuntu <ubuntu@ip-172-31-32-171.eu-west-2.compute.internal>
2020-11-21 00:05:01 +0000

    add aws and data dependency stuff

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 from rocker/shiny:3.6.1
+
 RUN apt-get update
 RUN apt-get -y install libssl-dev libgdal-dev libproj-dev libgeos-dev
+
+RUN apt-get install -y libpython3-dev python3-pip
+RUN pip3 install boto3 awscli
 
 RUN Rscript -e "install.packages(\
     c(\
@@ -12,17 +16,24 @@ RUN Rscript -e "install.packages(\
         'readr', \
         'tidyr', \
         'ggplot2', \
-        'rmarkdown'\
+        'rmarkdown',\
+        'rvest',\
+        'devtools',\
+        'magrittr',\
+        'here'\
     ),\
     repos = c(CRAN = 'http://cran.rstudio.com')\
     )"
 
-COPY ./civil_service_jobs_explorer /srv/shiny-server/
+RUN rm -r /srv/shiny-server/*
+COPY ./civil_service_jobs_explorer/ /srv/shiny-server/
 
-COPY shiny-server.sh /usr/bin/shiny-server.sh
+# COPY ./CivilServiceR/ /srv/shiny-server/CivilServiceR/
+# RUN Rscript -e "devtools::install_local('/srv/shiny-server/CivilServiceR/')"
+# CMD ["R"]
 
-RUN [ "chmod", "+x", "/usr/bin/shiny-server.sh"]
 RUN echo "preserve_logs true;" >> /etc/shiny-server/shiny-server.conf
 
+COPY shiny-server.sh /usr/bin/shiny-server.sh
+RUN [ "chmod", "+x", "/usr/bin/shiny-server.sh"]
 CMD ["/usr/bin/shiny-server.sh"]
-
--- shiny-server.sh
+++ shiny-server.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
+#remove toy examples from shiny server
 # Make sure the dir for individual app logs exists
 # sudo mkdir -p /var/log/shiny-server
 # chown shiny.shiny /var/log/shiny-server
-
 # if ["$APPLICATION_LOGS_TO_STDOUT" != "false"];
 #     then
 # exec xtail /var/log/shiny-server/
 # fi
+aws configure set default.region eu-west-2
+# export AWS_ACCESS_KEY_ID=xxxx #DO NOT DO THIS IN PRODUCTION, SET VIA ROLE ATTACHED TO PROD MACHINE
+# export AWS_SECRET_ACCESS_KEY=xxxxx
 
-# aws configure set default.region eu-west-2
+# Next para is all for getting data into the bucket which we'll do somewhere else
+# username=$(aws ssm get-parameter --name '/CivilServiceJobsExplorer/Toby/csjemail' --output text --query Parameter.Value)
+# password=$(aws ssm get-parameter --name '/CivilServiceJobsExplorer/Toby/csjpassword' --output text --query Parameter.Value)
+# rm /srv/shiny-server/CivilServiceR/user_name_and_password.R
+# touch /srv/shiny-server/CivilServiceR/user_name_and_password.R
+# echo "username = '$username'" >> /user_name_and_password.R
+# echo "password = '$password'" >> /user_name_and_password.R
+# cat /srv/shiny-server/CivilServiceR/user_name_and_password.R #can remove this
+# sudo Rscript -e "CivilServiceR::update_database()"
 
-# bucket_name=(aws ssm get-parameter --name 'CivilServiceJobsBucket')
-# aws s3 cp s3://${bucket_name}/data/ /srv/shiny-server/app_name/data/
-ls /srv/shiny-server/
+bucket_name=$(aws ssm get-parameter --name '/CivilServiceJobsExplorer/BucketName' --output text --query Parameter.Value)
+mkdir /srv/shiny-server/data/
+aws s3 cp s3://${bucket_name}/data/ /srv/shiny-server/data/ --recursive
+ls /srv/shiny-server/data/ #can remove this 
+
 env > /home/shiny/.Renviron
 chown shiny.shiny /home/shiny/.Renviron
+
 exec shiny-server 2>&1 &
 exec xtail /var/log/shiny-server/
